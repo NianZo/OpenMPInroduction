12a93b6052c7a361f433ef2b9ad9b764eef90301
NianZo <nicholas.angle@gmail.com>
2021-10-30 22:28:15 -0700

    Refactored to solve synchronization problem with #pragma omp atomic.

--- IntegrateToPi/IntegrateToPi.cpp
+++ IntegrateToPi/IntegrateToPi.cpp
@@ -18,34 +18,29 @@ constexpr int NUM_THREADS = 8;
 int main()
 {
 	omp_set_num_threads(NUM_THREADS);
-	std::array<double, NUM_THREADS> sum;
+	double sum;
 
 	constexpr double step = 1.0 / static_cast<double>(NUM_STEPS);
 
 	assert(NUM_STEPS % omp_get_num_threads() == 0); // For simplicity I am assuming that numThreads evenly divides NUM_STEPS
 
 	const double startTime = omp_get_wtime();
-	int actualThreadCount;
 #pragma omp parallel
 	{
 		double localSum = 0.0;
 
 		const uint32_t stepsPerThread = static_cast<uint32_t>(NUM_STEPS / omp_get_num_threads());
 		const uint32_t threadId = static_cast<uint32_t>(omp_get_thread_num());
-		if (threadId == 0)
-		{
-			actualThreadCount = omp_get_num_threads();
-		}
 		for (uint i = threadId * stepsPerThread; i < (threadId + 1) * stepsPerThread; i++)
 		{
 			const double x = (i + 0.5) * step;
 			localSum += 4.0 / (1.0 + x * x);
 		}
-		sum[threadId] = localSum;
+#pragma omp atomic // Atomic read-modify-write update of shared resource 'sum'
+		sum += localSum;
 	}
-	double finalSum = std::accumulate(sum.begin(), sum.begin() + actualThreadCount, 0.0);
 	const double endTime = omp_get_wtime();
-	const double pi = step * finalSum;
+	const double pi = step * sum;
 	std::cout << "Integral value: " << pi << std::endl;
 	std::cout << "True value of pi: " << std::numbers::pi << std::endl;
 	std::cout << "Calculation time: " << endTime - startTime << std::endl;
